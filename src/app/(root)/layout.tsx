917f3ee8a20cd8ac3b20ead264661a3162bbadb4
zakcore-g <160198082+zakcore-g@users.noreply.github.com>
2024-12-24 04:04:50 +0500

    start

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+export const dynamic = 'force-dynamic'
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      {children}
+    </main>
+  );
+}
